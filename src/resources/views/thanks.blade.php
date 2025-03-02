a28038b43af34dcfb6e9ff830b7021fea79692c9
saki <saki.3.30.s@gmail.com>
2025-03-02 23:54:18 +0900

    開発途中

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FashionabilyLate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
+
+<body>
+    <div class="thanks__page">
+        <div class="thanks__background">
+            <p class="thanks__background-text">Thank you</p>
+        </div>
+        <div class="thanks__content">
+            <div class="thanks__heading">
+                <h2>お問い合わせありがとうございました</h2>
+            </div>
+            <a class="thanks__btn" href="/">HOME</a>
+        </div>
+    </div>
+</body>
+
+</html>
