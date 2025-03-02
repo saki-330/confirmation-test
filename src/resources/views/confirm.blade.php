a959e8c92cad831b26468220bb7ac5dff67e4b29
saki <saki.3.30.s@gmail.com>
2025-03-02 23:09:13 +0900

    開発途中

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsection
+
+@section('content')
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Confirm</h2>
+    </div>
+    <form class="form">
+        <div class="confirm-table">
+            <table class="confirm-table__inner">
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お名前</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="name" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">性別</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="gender" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">メールアドレス</th>
+                    <td class="confirm-table__text">
+                        <input type="email" name="email" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">電話番号</th>
+                    <td class="confirm-table__text">
+                        <input type="tel" name="tel" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">住所</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="adress1" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">建物名</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="adress2" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせの種類</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="type" value="サンプルテキスト">
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせ内容</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="content" value="サンプルテキスト">
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit">送信</button>
+        </div>
+        <a class="form__correct" href="/">修正</a>
+    </form>
+    
+</div>
