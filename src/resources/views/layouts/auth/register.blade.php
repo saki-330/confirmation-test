4392030e556740ea7340dc117208461ce71e6f9a
saki <saki.3.30.s@gmail.com>
2025-03-03 11:01:28 +0900

    開発途中

---
+++ src/resources/views/layouts/auth/register.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('nav')
+<nav>
+    <ul class="header-nav">
+        <li class="header-nav__item">
+            <a class="header-nav__link" href="/login">login</a>
+        </li>
+    </ul>
+</nav>
+@endsection
+
+@section('content')
+<div class="register-form__content">
+    <div class="register-form__heading">
+        <h2>Register</h2>
+    </div>
+    <div class="register-form__group-wrapper">
+        <form class="form">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">お名前</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="name" value="{{ old('name') }}" placeholder="例: 山田　太郎">
+                    </div>
+                    <div class="form__error">
+                        @error('name')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">メールアドレス</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="email" name="email" value="{{ old('email') }}" placeholder="例: test@example.com">
+                    </div>
+                    <div class="form__error">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">パスワード</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="password" name="password" placeholder="例: coachtech1106">
+                    </div>
+                    <div class="form__error">
+                        @error('password')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit" type="submit">登録</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
